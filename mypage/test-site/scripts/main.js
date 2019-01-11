a6f70f5b0a82a715f50daf8e0f1e57ccf0d6c22d
Carolina Sottovia <46378422+carolsottovia@users.noreply.github.com>
2019-01-11 21:26:44 -0200

    Update main.js

--- mypage/test-site/scripts/main.js
+++ mypage/test-site/scripts/main.js
@@ -17,14 +17,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Hello, ' + myName;
+  myHeading.innerHTML = 'Hello, ' +        myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Hello, ' + storedName;
+  myHeading.innerHTML = 'Hello, ' +        storedName;
 }
 
 myButton.onclick = function() {
